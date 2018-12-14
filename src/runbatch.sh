df86bd1472d802c5492ed21044db53d86cbe9790
doggydigit <matthias.chinyen.tsai@gmail.com>
2018-12-14 16:16:57 +0100

    Corrected minor things in bash script to launch montecarlo jobs

--- src/runbatch.sh
+++ src/runbatch.sh
@@ -5,4 +5,6 @@ k="_r"
 for job in $(seq 1 16)
 do
     eval 'nohup /home/tsai/anaconda2/envs/p2/bin/python2.7 montesearch.py >> log_s$1_j$job$k$2.out 2>&1 &'
+    echo "Launched job $job"
+    sleep 2s
 done
